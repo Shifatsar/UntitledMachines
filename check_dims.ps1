81887a4dd9dccb61ec5547752b137a6ffffeb84c
shifatsar <shifatsar@gmail.com>
2026-04-26 12:24:04 +0500

    refactor: simplify asset paths, remove unused mouse sprites, and delete build/development scripts

--- check_dims.ps1
+++
@@ -1,4 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$img = [System.Drawing.Image]::FromFile("d:\Projects\UntitledMachines\mouse_sprites_3.png")
-Write-Host "$($img.Width)x$($img.Height)"
-$img.Dispose()
